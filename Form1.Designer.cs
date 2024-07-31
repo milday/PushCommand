b6e889cedab0dd631daf3f35c9c4b8273273f964
milday <103111108+milday@users.noreply.github.com>
2024-08-01 02:50:34 +0800

    V1

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace PushCommand
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.hostnameTextBox = new System.Windows.Forms.TextBox();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.commandTextBox = new System.Windows.Forms.TextBox();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.clearLogButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lapsCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // hostnameTextBox
+            // 
+            this.hostnameTextBox.Location = new System.Drawing.Point(98, 33);
+            this.hostnameTextBox.Name = "hostnameTextBox";
+            this.hostnameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.hostnameTextBox.TabIndex = 0;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(98, 59);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.TabIndex = 1;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(98, 85);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 2;
+            // 
+            // commandTextBox
+            // 
+            this.commandTextBox.Location = new System.Drawing.Point(98, 130);
+            this.commandTextBox.Name = "commandTextBox";
+            this.commandTextBox.Size = new System.Drawing.Size(399, 20);
+            this.commandTextBox.TabIndex = 3;
+            // 
+            // executeButton
+            // 
+            this.executeButton.Location = new System.Drawing.Point(98, 200);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(100, 47);
+            this.executeButton.TabIndex = 4;
+            this.executeButton.Text = "Go!!";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Hostname :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Username :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password :";
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(34, 253);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logTextBox.Size = new System.Drawing.Size(463, 136);
+            this.logTextBox.TabIndex = 10;
+            // 
+            // clearLogButton
+            // 
+            this.clearLogButton.Location = new System.Drawing.Point(422, 395);
+            this.clearLogButton.Name = "clearLogButton";
+            this.clearLogButton.Size = new System.Drawing.Size(75, 23);
+            this.clearLogButton.TabIndex = 11;
+            this.clearLogButton.Text = "Clear Log";
+            this.clearLogButton.UseVisualStyleBackColor = true;
+            this.clearLogButton.Click += new System.EventHandler(this.clearLogButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Command :";
+            // 
+            // lapsCheckBox
+            // 
+            this.lapsCheckBox.AutoSize = true;
+            this.lapsCheckBox.Location = new System.Drawing.Point(98, 107);
+            this.lapsCheckBox.Name = "lapsCheckBox";
+            this.lapsCheckBox.Size = new System.Drawing.Size(53, 17);
+            this.lapsCheckBox.TabIndex = 13;
+            this.lapsCheckBox.Text = "LAPS";
+            this.lapsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 443);
+            this.Controls.Add(this.lapsCheckBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.clearLogButton);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.commandTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.hostnameTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RunCommand - MAELKK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox hostnameTextBox;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox commandTextBox;
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Button clearLogButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox lapsCheckBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+using System.Net;
+using System.Net.Sockets;
+using System.Security;
+using System.Windows.Forms;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.IO;
+
+namespace PushCommand
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            commandTextBox.Text = "";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            logTextBox.ReadOnly = true;
+            lapsCheckBox.CheckedChanged += LapsCheckBox_CheckedChanged;
+            LapsCheckBox_CheckedChanged(null, null);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void executeButton_Click(object sender, EventArgs e)
+        {
+            string hostname = hostnameTextBox.Text;
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+            string command = commandTextBox.Text;
+
+            // Resolve IP to hostname if needed
+            if (IsIPAddress(hostname))
+            {
+                hostname = GetHostNameFromIPAddress(hostname);
+                if (string.IsNullOrEmpty(hostname))
+                {
+                    MessageBox.Show("Failed to resolve IP address to hostname.");
+                    return;
+                }
+            }
+
+            if (lapsCheckBox.Checked)
+            {
+                if (IsIPAddress(hostname))
+                {
+                    MessageBox.Show("Please use hostname instead of IP address when LAPS is enabled.");
+                    return;
+                }
+
+                password = GetLapsPassword(GetShortHostName(hostname));
+                if (string.IsNullOrEmpty(password))
+                {
+                    MessageBox.Show("Failed to retrieve password from LAPS.");
+                    return;
+                }
+
+                username = "admdesktop";
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(password))
+                {
+                    MessageBox.Show("Password cannot be empty when LAPS is not enabled.");
+                    return;
+                }
+            }
+
+            if (IsLocalMachine(hostname))
+            {
+                string result = ExecuteLocalCommand(command);
+                logTextBox.AppendText(result + Environment.NewLine);
+            }
+            else
+            {
+                string result = ExecuteRemoteCommandWithWinRMCheck(hostname, username, password, command);
+                logTextBox.AppendText(result + Environment.NewLine);
+            }
+        }
+
+        private bool IsLocalMachine(string hostname)
+        {
+            string localHostname = Environment.MachineName;
+            return string.Equals(hostname, localHostname, StringComparison.OrdinalIgnoreCase);
+        }
+
+        private string ExecuteLocalCommand(string command)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/c {command}",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(startInfo))
+                {
+                    result = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        result += "\nError: " + error;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result = $"Error executing local command: {ex.Message}\n{ex.StackTrace}";
+            }
+
+            return result;
+        }
+
+        private bool IsIPAddress(string hostname)
+        {
+            return IPAddress.TryParse(hostname, out _);
+        }
+
+        private string GetHostNameFromIPAddress(string ipAddress)
+        {
+            try
+            {
+                IPHostEntry hostEntry = Dns.GetHostEntry(ipAddress);
+                return hostEntry.HostName;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error resolving IP address: " + ex.Message);
+                return null;
+            }
+        }
+
+        private string GetShortHostName(string fullHostName)
+        {
+            if (fullHostName.Contains("."))
+            {
+                return fullHostName.Split('.')[0];
+            }
+
+            return fullHostName;
+        }
+
+        private string GetLapsPassword(string hostname)
+        {
+            string password = string.Empty;
+
+            try
+            {
+                using (PowerShell ps = PowerShell.Create())
+                {
+                    // Set ExecutionPolicy to allow script to run
+                    ps.AddCommand("Set-ExecutionPolicy")
+                      .AddParameter("ExecutionPolicy", "Bypass")
+                      .AddParameter("Scope", "Process")
+                      .AddParameter("Force", true);
+                    ps.Invoke();
+
+                    // Define paths for module
+                    string modulePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "WindowsPowerShell", "Modules", "AdmPwd.PS");
+                    string sourcePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "AdmPwd.PS");
+
+                    // Check if the module is installed and copy if necessary
+                    ps.AddScript($@"
+                if (-not (Test-Path -Path '{modulePath}')) {{
+                    if (Test-Path -Path '{sourcePath}') {{
+                        Copy-Item -Recurse -Path '{sourcePath}' -Destination '{modulePath}'
+                    }} else {{
+                        throw 'Source path does not exist.'
+                    }}
+                }}
+            ");
+                    ps.Invoke();
+
+                    if (ps.HadErrors)
+                    {
+                        foreach (var error in ps.Streams.Error)
+                        {
+                            MessageBox.Show("Error during module setup: " + error.ToString());
+                        }
+                        return string.Empty;
+                    }
+
+                    // Import the module
+                    ps.AddScript("Import-Module -Name 'AdmPwd.PS' -ErrorAction Stop");
+                    ps.Invoke();
+
+                    if (ps.HadErrors)
+                    {
+                        foreach (var error in ps.Streams.Error)
+                        {
+                            MessageBox.Show("Error importing module: " + error.ToString());
+                        }
+                        return string.Empty;
+                    }
+
+                    // Retrieve the password
+                    ps.AddScript($"Get-AdmPwdPassword -ComputerName {hostname}");
+                    var results = ps.Invoke();
+
+                    if (ps.HadErrors)
+                    {
+                        foreach (var error in ps.Streams.Error)
+                        {
+                            MessageBox.Show("Error retrieving password: " + error.ToString());
+                        }
+                        return string.Empty;
+                    }
+
+                    if (results.Count > 0)
+                    {
+                        password = results[0].Properties["Password"].Value.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error retrieving password: " + ex.Message);
+            }
+
+            return password;
+        }
+
+
+
+        private string ExecuteRemoteCommandWithWinRMCheck(string hostname, string username, string password, string command)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                ConnectionOptions options = new ConnectionOptions
+                {
+                    Username = $"{hostname}\\{username}",
+                    Password = password,
+                    EnablePrivileges = true,
+                    Impersonation = ImpersonationLevel.Impersonate
+                };
+
+                ManagementScope scope = new ManagementScope($"\\\\{hostname}\\root\\cimv2", options);
+                scope.Connect();
+
+                if (!IsWinRMEnabled(scope))
+                {
+                    result = EnableWinRM(scope);
+                    result += ConfigureTrustedHosts(scope);
+                }
+
+                result += ExecuteRemoteCommand(hostname, username, password, command);
+            }
+            catch (Exception ex)
+            {
+                result = $"Error: {ex.Message}\n{ex.StackTrace}";
+                Debug.WriteLine($"Exception occurred: {ex}");
+            }
+
+            return result;
+        }
+
+        private bool IsWinRMEnabled(ManagementScope scope)
+        {
+            try
+            {
+                ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_Service WHERE Name = 'WinRM'");
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+                ManagementObjectCollection queryCollection = searcher.Get();
+
+                foreach (ManagementObject service in queryCollection)
+                {
+                    if (service["State"].ToString().ToLower() == "running")
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error checking WinRM status: " + ex.Message);
+            }
+
+            return false;
+        }
+
+        private string EnableWinRM(ManagementScope scope)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                ManagementClass processClass = new ManagementClass(scope, new ManagementPath("Win32_Process"), new ObjectGetOptions());
+                ManagementBaseObject inParams = processClass.GetMethodParameters("Create");
+                inParams["CommandLine"] = "cmd /c winrm quickconfig -force";
+
+                ManagementBaseObject outParams = processClass.InvokeMethod("Create", inParams, null);
+
+                result = $"WinRM enabled. Process ID: {outParams["processId"]}\nReturn Value: {outParams["returnValue"]}\n";
+            }
+            catch (Exception ex)
+            {
+                result = $"Error enabling WinRM: {ex.Message}\n{ex.StackTrace}";
+            }
+
+            return result;
+        }
+
+        private string ConfigureTrustedHosts(ManagementScope scope)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                ManagementClass processClass = new ManagementClass(scope, new ManagementPath("Win32_Process"), new ObjectGetOptions());
+                ManagementBaseObject inParams = processClass.GetMethodParameters("Create");
+                inParams["CommandLine"] = "cmd /c winrm set winrm/config/client '@{TrustedHosts=\"*\"}'";
+
+                ManagementBaseObject outParams = processClass.InvokeMethod("Create", inParams, null);
+
+                result = $"TrustedHosts configured. Process ID: {outParams["processId"]}\nReturn Value: {outParams["returnValue"]}\n";
+            }
+            catch (Exception ex)
+            {
+                result = $"Error configuring TrustedHosts: {ex.Message}\n{ex.StackTrace}";
+            }
+
+            return result;
+        }
+
+        private string ExecuteRemoteCommand(string hostname, string username, string password, string command)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                using (PowerShell ps = PowerShell.Create())
+                {
+                    // Set ExecutionPolicy to allow script to run
+                    ps.AddCommand("Set-ExecutionPolicy")
+                      .AddParameter("ExecutionPolicy", "Bypass")
+                      .AddParameter("Scope", "Process")
+                      .AddParameter("Force", true);
+                    ps.Invoke();
+
+                    string script = $"Invoke-Command -ComputerName {hostname} -Credential (New-Object System.Management.Automation.PSCredential('{hostname}\\{username}', (ConvertTo-SecureString '{password}' -AsPlainText -Force))) -ScriptBlock {{ cmd /c {command} }}";
+
+                    ps.AddScript(script);
+
+                    var results = ps.Invoke();
+
+                    foreach (var output in results)
+                    {
+                        result += output.ToString() + Environment.NewLine;
+                    }
+
+                    if (ps.HadErrors)
+                    {
+                        foreach (var error in ps.Streams.Error)
+                        {
+                            result += "Error: " + error.ToString() + Environment.NewLine;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result = $"Error executing remote command: {ex.Message}\n{ex.StackTrace}";
+            }
+
+            return result;
+        }
+
+        private void clearLogButton_Click(object sender, EventArgs e)
+        {
+            logTextBox.Clear();
+        }
+
+        private void LapsCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (lapsCheckBox.Checked)
+            {
+                usernameTextBox.Text = "admdesktop";
+                usernameTextBox.Enabled = false;
+                passwordTextBox.Enabled = false;
+                passwordTextBox.Text = string.Empty;
+            }
+            else
+            {
+                usernameTextBox.Enabled = true;
+                passwordTextBox.Enabled = true;
+            }
+        }
+    }
+}
